122cfff9877b3c8c08e9a39aacc0c3806d7522a0
Bastiaan Jacobs <bas-jacobs@hotmail.com>
2026-02-03 21:22:53 +0100

    New Personal Website

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
-export default {
-  base: '/',
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Website/',
+  server: {
+    port: 6123,
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
+    host: true,
+    strictPort: true,
+  },
+})
